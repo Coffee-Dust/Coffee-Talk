2f8b46e7a23c23878cf165a722069282c19ffda9
github username <d24dust@hotmail.com>
2020-09-24 15:35:52 -0400

    add: relational macros

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  belongs_to :poster, class_name: "User"
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: "poster_id"
+  has_many :comments, foreign_key: "commentor_id"
+  has_many :commented_posts, through: :comments, source: "post"
 end
